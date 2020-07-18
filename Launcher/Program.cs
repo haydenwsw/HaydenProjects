bea9840d0ce80928f87a59348ca9062eba7d6b51
Hayden <43462177+Theoogamar@users.noreply.github.com>
2020-07-18 17:13:13 +1000

    Added launcher

---
+++ Launcher/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Launcher
+{
+    class Program
+    {
+        private const string path = @"..\..\..\..\CarsWebApp\bin\Release\netcoreapp3.1\publish\CarsWebApp.exe";
+
+        static void Main(string[] args)
+        {
+            Process.Start(path);
+            Process.Start("explorer", "https://localhost:5001/");
+        }
+    }
+}
